adaa7f608019c53424d54ea851936d56695b68b8
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-24 15:05:35 +0500

    Add Rakefile,update Gemfile, add Product model

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/activerecord'
 
+set :database, {adapter: "sqlite3", database: "shop.db"}
+
+class Product < ActiveRecord::Base 
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
