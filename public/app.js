2930868a6d53d2769e4e42a43cbbd27299ab0880
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-26 05:59:54 +0500

    pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -3,9 +3,14 @@ function something()
 {
     var x = window.localStorage.getItem('aaa');
 
-    x = x * 1 + 1;
+     x = x * 1 + 1;
 
     window.localStorage.setItem('aaa', x);
     
     alert(x);
+}
+
+function add_to_cart(id)
+{
+    alert('You added pizza with id:' + id);
 }
