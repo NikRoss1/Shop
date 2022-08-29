c1aea14a5aadc7d6b6395929672ab52b312d59fe
Nikolay Panov <nik.cmkw@yandex.ru>
2022-08-30 03:12:32 +0500

    Save orders to database. display "thank you", clear localStoreg

--- app.rb
+++ app.rb
@@ -20,6 +20,11 @@ get "/about" do
 	erb :about
 end
 
+post '/place_order' do
+  @order = Order.create params[:orders]
+  erb :order_placed
+end
+
 post '/cart' do
   @orders_input = params[:orders]
 
